178b66ec4a2922bae9a2466946044ade52eccf84
Matthias <matthiasou@yahoo.fr>
2017-05-22 13:53:15 +0200

    poste dans le bon sens

--- src/SosBundle/Controller/SearchController.php
+++ src/SosBundle/Controller/SearchController.php
@@ -38,7 +38,7 @@ class SearchController extends Controller
             $alreadyVille = $request->get('ville');
             if (empty($alreadyVille['latitude'])) 
             {
-                $geocoder = 'https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyCs2SKJn7FjYzd1SIBdAZgsVw1DrXtEJno&address=%s&sensor=false';
+                $geocoder = 'https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyBzgfrxmpReuCPy7rGePa9B-be2Fj5_qI4&address=%s&sensor=false';
                 $query = sprintf($geocoder, urlencode($_POST['ville']));
                 $result = json_decode(file_get_contents($query));
 
