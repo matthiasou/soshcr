8a2d381bcf9be03edda7544dcc4c98bb63688b7a
bmagneCesi <bmagne@bobbies.fr>
2017-05-22 14:24:46 +0200

    changement route si pas de usercritere trouvés

--- src/SosBundle/Controller/UserController.php
+++ src/SosBundle/Controller/UserController.php
@@ -30,41 +30,48 @@ class UserController extends Controller
         $postesArr = array();
         $contratsArr = array();
 
-        foreach ($user->criteres as $key => $critere) {
+        if (!$user->getCriteres()->isEmpty())
+        {
+            foreach ($user->criteres as $key => $critere) {
 
-            $formations = $critere->getFormation();
-            $contrats = $critere->getContrat();
-            $poste = $critere->getPoste();
-            $experience = $critere->getExperience();
-            $duree = $critere->getDuree();
+                $formations = $critere->getFormation();
+                $contrats = $critere->getContrat();
+                $poste = $critere->getPoste();
+                $experience = $critere->getExperience();
+                $duree = $critere->getDuree();
 
-            $formationsArr = array();
-            foreach ($formations as $key => $formation) {
-                $formationsArr[] = $formation->getLibelle();
+                $formationsArr = array();
+                foreach ($formations as $key => $formation) {
+                    $formationsArr[] = $formation->getLibelle();
+                }
+
+                $dureesArr = array();
+                foreach ($duree as $key => $value) {
+                    $dureesArr[] = $value->getLibelle();    
+                }
+                $postesArr[] = array('poste' => $poste->getLibelle(), 'experience' => $experience->getLibelle());
+                $contratsArr[] = array('contrat' => $contrats->getLibelle(), 'duree' => $dureesArr);
+                $anglaisArr[] = $critere->getNiveauAnglais()->getLibelle();
+                $disponibilitesArr[] = json_decode($critere->getDisponibilites());
             }
 
-            $dureesArr = array();
-            foreach ($duree as $key => $value) {
-                $dureesArr[] = $value->getLibelle();    
-            }
-            $postesArr[] = array('poste' => $poste->getLibelle(), 'experience' => $experience->getLibelle());
-            $contratsArr[] = array('contrat' => $contrats->getLibelle(), 'duree' => $dureesArr);
-            $anglaisArr[] = $critere->getNiveauAnglais()->getLibelle();
-            $disponibilitesArr[] = json_decode($critere->getDisponibilites());
+            $disponibilitesArr = array_unique($disponibilitesArr, SORT_REGULAR);
+            $postesArr = array_unique($postesArr, SORT_REGULAR);
+            $contratsArr = array_unique($contratsArr, SORT_REGULAR);
+            $formationsArr = array_unique($formationsArr);
+            $anglaisArr = array_unique($anglaisArr);
+            $userCriteres = array('formations' => $formationsArr, 'postes' => $postesArr, 'anglais' => $anglaisArr[0], 'contrats' => $contratsArr, 'disponibilite' => $disponibilitesArr[0]);
+
+            $now = new \DateTime('now');
+            $age = $user->getDateNaissance();
+            $userAge = $now->diff($age);
+            
+            return $this->render('SosBundle:User:publicProfil.html.twig', array("user" => $user, 'userCriteres' => $userCriteres, 'userAge' => $userAge->y));  
         }
-
-        $disponibilitesArr = array_unique($disponibilitesArr, SORT_REGULAR);
-        $postesArr = array_unique($postesArr, SORT_REGULAR);
-        $contratsArr = array_unique($contratsArr, SORT_REGULAR);
-        $formationsArr = array_unique($formationsArr);
-        $anglaisArr = array_unique($anglaisArr);
-        $userCriteres = array('formations' => $formationsArr, 'postes' => $postesArr, 'anglais' => $anglaisArr[0], 'contrats' => $contratsArr, 'disponibilite' => $disponibilitesArr[0]);
-
-        $now = new \DateTime('now');
-        $age = $user->getDateNaissance();
-        $userAge = $now->diff($age);
+        else
+        {
+            return $this->redirectToRoute('usercriteres_step_1');
+        }
         
-
-        return $this->render('SosBundle:User:publicProfil.html.twig', array("user" => $user, 'userCriteres' => $userCriteres, 'userAge' => $userAge->y));
     }
 }
