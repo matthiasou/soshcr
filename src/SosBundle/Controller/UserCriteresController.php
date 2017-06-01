9c950309567bf2d71632744e7c4f3c8f2b1eb67f
Matthias <matthiasou@yahoo.fr>
2017-06-01 20:40:29 +0200

    resultat

--- src/SosBundle/Controller/UserCriteresController.php
+++ src/SosBundle/Controller/UserCriteresController.php
@@ -120,6 +120,7 @@ class UserCriteresController extends Controller
             $geocoder = 'http://maps.googleapis.com/maps/api/geocode/json?address=%s&sensor=false';
             $query = sprintf($geocoder, urlencode($_POST['ville']));
             $result = json_decode(file_get_contents($query));
+            dump($result);
             if ($result == NULL || $result == "" ||  $result->status == "ZERO_RESULTS" || $result->status == "INVALID_REQUEST"  || $result->status == "REQUEST_DENIED" ){
                 return $this->render('SosBundle:UserCriteres:step3.html.twig', array('error' => 'ville'));
             }else{
