4ba083d10e51dd8f4265a2b847f1edbe744dcfc6
nsken <maurayflorian@gmail.com>
2017-06-11 22:46:22 +0200

    Payplug correction

--- src/SosBundle/Controller/OrdersController.php
+++ src/SosBundle/Controller/OrdersController.php
@@ -4,135 +4,63 @@ namespace SosBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
-use JMS\Payment\CoreBundle\Form\ChoosePaymentMethodType;
-use SosBundle\Entity\Order;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\HttpFoundation\Request;
-use JMS\Payment\CoreBundle\PluginController\Result;
-use JMS\Payment\CoreBundle\Plugin\Exception\Action\VisitUrl;
-use JMS\Payment\CoreBundle\Plugin\Exception\ActionRequiredException;
+use Payplug\Resource\Payment;
+use Payplug\Payplug;
 
-/**
- * @Route("/orders")
- */
 class OrdersController extends Controller
 {
-
-/**
- * @Route("/new/{amount}")
- */
-public function newAction($amount)
-{
-    $em = $this->getDoctrine()->getManager();
-
-    $order = new Order($amount);
-    $em->persist($order);
-    $em->flush();
-    return $this->redirect($this->generateUrl('show', [
-        'id' => $order->getId(),
-    ]));
-}
-/**
- * @Route("/{id}/show")
- * @Template
- */
-public function showAction(Request $request, Order $order)
-{
-	$config = [
-    'paypal_express_checkout' => [
-        'return_url' => "https://soshcr.fr/dev/soshcr2/web/app_dev.php/".$order->getId()."/payment/complete",
-        'cancel_url' => "https://soshcr.fr/dev/soshcr2/web/app_dev.php/".$order->getId()."/payment/cancel"
-    ],
-];
-	$form = $this->createForm(ChoosePaymentMethodType::class, null, [
-	    'amount'   => '00.01',
-	    'currency' => 'EUR',
-	    'allowed_methods' => ['paypal_express_checkout'],
-        'predefined_data' => $config,
-	]);
-	$form->handleRequest($request);
-
-    if ($form->isSubmitted() && $form->isValid()) {
-
-        $ppc = $this->get('payment.plugin_controller');
-        $ppc->createPaymentInstruction($instruction = $form->getData());
-
-        $order->setPaymentInstruction($instruction);
-
+    public function paymentAction()
+    {
         $em = $this->getDoctrine()->getManager();
-        $em->persist($order);
-        $em->flush($order);
-        $idorder = $order->getId();
-        return $this->redirect($this->generateUrl('paymentCreate', [
-            'id' => $order->getId(),
-        ]));
-    }
-    return $this->render('SosBundle:Orders:show.html.twig', array(
-        'order' => $order,
-        'form' => $form->createView()
-    ));
-}
-private function createPayment($order)
-{
-    $instruction = $order->getPaymentInstruction();
-    $pendingTransaction = $instruction->getPendingTransaction();
-
-    if ($pendingTransaction !== null) {
-        return $pendingTransaction->getPayment();
-    }
-
-    $ppc = $this->get('payment.plugin_controller');
-    $amount = $instruction->getAmount() - $instruction->getDepositedAmount();
-
-    return $ppc->createPayment($instruction->getId(), $amount);
-}
-
-/**
- * @Route("/{id}/payment/create")
- */
-public function paymentCreateAction(Order $order)
-{
-    $payment = $this->createPayment($order);
-    $ppc = $this->get('payment.plugin_controller');
-    $result = $ppc->approveAndDeposit($payment->getId(), $payment->getTargetAmount());
-
-    if ($result->getStatus() === Result::STATUS_SUCCESS) {
+        $user = $this->get('security.token_storage')->getToken()->getUser();
+        Payplug::setSecretKey('sk_test_1Ku7CEQ0UC5sT5y4N2ZBR2');
+        $parameters = [
+          'amount'   => 10 *10, 
+          'currency' => 'EUR',
+          'customer' => [ // optionnel
+               'email'      => $user->getEmail(),
+               'first_name' => $user->getPrenom(),
+               'last_name'  => $user->getNom(),
+          ],
+          'metadata' => [ 
+               'order_id' => 42,
+               'customer_id' => $user->getId(),
+          ],
+          'hosted_payment' => [
+               'return_url' => 'https://soshcr.fr/dev/soshcr2/web/app_dev.php/payment/success/42',
+               'cancel_url' => 'https://soshcr.fr/dev/soshcr2/web/app_dev.php/payment/error/42',
+          ]
+        ];
         
-        return $this->redirect($this->generateUrl('paymentComplete', array('id' => $id)));
+        try {
+    $payment = Payment::create($parameters);
+    $pay = $payment->hosted_payment->payment_url;
+    } catch (ConnectionException $e) {
+        $this->log("Connection  with the PayPlug API failed.");
+    } catch (InvalidPaymentException $e) {
+        $this->log("Payment object provided is invalid.");
+    } catch (UndefinedAttributeException $e) {
+        $this->log("Requested attribute is undefined.");
+    } catch (HttpException $e) {
+        $this->log("Http errors.");
+    } catch (PayplugException $e) {
+        $this->log($e->getMessage());
+    } catch (\Exception $e) {
+        $this->log($e->getMessage());
     }
-    if ($result->getStatus() === Result::STATUS_PENDING) {
-    $ex = $result->getPluginException();
-
-    if ($ex instanceof ActionRequiredException) {
-        $action = $ex->getAction();
-
-        if ($action instanceof VisitUrl) {
-            return $this->redirect($action->getUrl());
-        }
+        return $this->render('SosBundle:Dashboard:payment.html.twig', array('user' => $user, 'pay' => $pay));
     }
-	}
+    public function sucessAction()
+    {
+        return $this->render('SosBundle:Orders:sucess.html.twig');
 
-    //return $this->redirectToRoute('abonnement');
-    throw $result->getPluginException();
+    }
+    public function errorAction()
+    {
+        return $this->render('SosBundle:Orders:error.html.twig');
 
-    // In a real-world application you wouldn't throw the exception. You would,
-    // for example, redirect to the showAction with a flash message informing
-    // the user that the payment was not successful.
-}
-/**
- * @Route("/{id}/payment/complete")
- */
-public function paymentCompleteAction(Order $order)
-{
-    return new Response('Payment complete');
-}
-/**
- * @Route("/{id}/payment/cancel")
- */
-public function paymentCancelAction(Order $order)
-{
-    return new Response('Payment cancel');
-}
-
+    }
 }
